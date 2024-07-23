fbd2b87891ac762b04abb674e085b9dc1d7612dc
zachypng <zach.smith0121@gmail.com>
2024-07-23 14:23:12 -0400

    fix: env reference for prod

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -2,7 +2,12 @@ import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
 import { db } from './db/db';
 import { sessionTable, userTable } from './db/schema';
 import { Airtable } from 'arctic';
-import { AIRTABLE_CLIENT_ID, AIRTABLE_CLIENT_SECRET } from '$env/static/private';
+import {
+	AIRTABLE_CLIENT_ID,
+	AIRTABLE_CLIENT_SECRET,
+	BASE_URL,
+	VERCEL_URL
+} from '$env/static/private';
 import { Lucia } from 'lucia';
 import { dev } from '$app/environment';
 
@@ -11,7 +16,7 @@ const adapter = new DrizzleSQLiteAdapter(db, sessionTable, userTable);
 export const airtable = new Airtable(
 	AIRTABLE_CLIENT_ID,
 	AIRTABLE_CLIENT_SECRET,
-	'http://localhost:5173/login/airtable/callback'
+	!dev ? VERCEL_URL : BASE_URL + '/login/airtable/callback'
 );
 
 export const lucia = new Lucia(adapter, {
