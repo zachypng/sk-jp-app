71d034ac264e97b62f05e87c78b6fd85b15c92c3
zachypng <zach.smith0121@gmail.com>
2025-01-27 12:19:04 -0500

    names quickfix

--- src/routes/(app)/settings/schemas.ts
+++ src/routes/(app)/settings/schemas.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const names = ['Christopher', 'Jacob', 'John B', 'Katie', 'Rob', 'Samuel', 'Zach'];
+export const names = ['Jaclyn', 'Jacob', 'Katie', 'Samuel', 'Zach'];
 
 export const nameForm = z.object({
 	name: z.string().min(2).max(50)
